3001fbac856af729b8fa7f3a1b7862fed8d0c1ca
christianholm030-commits <christianholm030@gmail.com>
2025-10-19 20:23:45 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,27 +1,17 @@
 const CACHE = 'calc-v1';
 const ASSETS = [
-  './',
-  './index.html',
-  './manifest.webmanifest',
-  './icons/icon-192.png',
-  './icons/icon-512.png'
+  '/Lommeregner-/',
+  '/Lommeregner-/index.html',
+  '/Lommeregner-/manifest.webmanifest',
+  '/Lommeregner-/sw.js',
+  '/Lommeregner-/icons/icon-192.png',
+  '/Lommeregner-/icons/icon-512.png'
 ];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
-  self.skipWaiting();
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
 });
-
-self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', (e) => {
+self.addEventListener('fetch', e => {
   e.respondWith(
     caches.match(e.request).then(r => r || fetch(e.request))
   );
